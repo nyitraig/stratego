2d19b87910f16a5c1e48fe2fa4015ffe0fc38751
Gergő Nyitrai <nyitraig+github@protonmail.com>
2020-06-21 23:37:58 +0200

    Updates

---
+++ precache-manifest.189db70c97df3b8274f6b707a3512562.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14847c7550391da3864d382fce06a569",
+    "url": "/stratego/index.html"
+  },
+  {
+    "revision": "3142da95f3b039ae9075",
+    "url": "/stratego/static/css/main.f7336ce1.chunk.css"
+  },
+  {
+    "revision": "a7022b85f2d3959e1187",
+    "url": "/stratego/static/js/2.f1f898b5.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/stratego/static/js/2.f1f898b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3142da95f3b039ae9075",
+    "url": "/stratego/static/js/main.1f01e38c.chunk.js"
+  },
+  {
+    "revision": "c72125b91d92b9b7b9b2",
+    "url": "/stratego/static/js/runtime-main.44230fc6.js"
+  },
+  {
+    "revision": "52e53c2af3f6f6c6aadb79be2ded59fd",
+    "url": "/stratego/static/media/logo.52e53c2a.png"
+  }
+]);
